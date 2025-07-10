bbc4b0af1bb4902a65e738fb988637b99cc53230
there080922 <theresia080299@gmail.com>
2025-07-10 16:19:27 +0800

    slebew

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,8 +50,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        // Jangan login otomatis, langsung redirect ke login
+        return redirect()->route('login')->with('status', 'Registration successful. Please login.');
     }
 }
